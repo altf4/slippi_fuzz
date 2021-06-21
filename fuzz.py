ad7b1acdd72c677ae23e84052f0fdb5ddf00fb16
AltF4 <altf4petro@gmail.com>
2021-06-20 20:15:59 -0700

    Chat fuzzing is ready

--- fuzz.py
+++ fuzz.py
@@ -30,18 +30,30 @@ else:
 print("using seed: ", seed)
 random.seed(seed)
 
-print(args.opponent)
+class Pad:
+    def __init__(self):
+        self.messageId = 0x80
+        self.frame = 0x00000000
+        self.playerIdx = 0x00
+        # Then contains N pads (each 8 bytes)
+        self.pads = [0x0000000000000000]
+
+class PadAck:
+    def __init__(self):
+        self.messageId = 0x81
+        self.frame = 0x00000000
+        self.playerIdx = 0x00
 
 class PlayerSelections:
     def __init__(self):
         self.messageId = 0x82
-        self.characterId = 0
+        self.characterId = 0x07
         self.characterColor = 0
-        self.isCharacterSelected = 0
+        self.isCharacterSelected = 1
         self.playerIdx = 0
-        self.stageId = 0
-        self.isStageSelected = 0
-        self.rngOffset = 0
+        self.stageId = 3
+        self.isStageSelected = 1
+        self.rngOffset = 0x12345678
         self.teamId = 0
 
     def randomize(self):
@@ -49,7 +61,7 @@ class PlayerSelections:
         self.characterId = random.getrandbits(8)
         self.characterColor = random.getrandbits(8)
         self.isCharacterSelected = random.getrandbits(8)
-        self.playerIdx = random.getrandbits(8)
+        self.playerIdx = random.getrandbits(1)
         self.stageId = random.getrandbits(16)
         self.isStageSelected = random.getrandbits(8)
         self.rngOffset = random.getrandbits(32)
@@ -68,42 +80,54 @@ class PlayerSelections:
         paramlist.append((self.teamId).to_bytes(1, byteorder='big'))
         return b"".join(paramlist)
 
-    # packet << static_cast<MessageId>(NP_MSG_SLIPPI_MATCH_SELECTIONS);
-	# packet << s.characterId << s.characterColor << s.isCharacterSelected;
-	# packet << s.playerIdx;
-	# packet << s.stageId << s.isStageSelected;
-	# packet << s.rngOffset;
-	# packet << s.teamId;
+class ConnSelected:
+    def __init__(self):
+        self.messageId = 0x83
+        # No other fields
+
+class ChatMessage:
+    def __init__(self):
+        self.messageId = 0x84
+        self.chatMessage = 0x00000000
+        self.playerIdx = 0x00
+
+    def randomize(self):
+        self.messageId = 0x84
+        self.chatMessage = random.getrandbits(32)
+        self.playerIdx = random.getrandbits(8)
+
+    def to_buffer(self):
+        paramlist = []
+        paramlist.append((self.messageId).to_bytes(1, byteorder='big'))
+        paramlist.append((self.chatMessage).to_bytes(4, byteorder='big'))
+        paramlist.append((self.playerIdx).to_bytes(1, byteorder='big'))
+        return b"".join(paramlist)
 
 # Connect to MM server
 host = enet.Host(None, 1, 0, 0)
 peer = host.connect(enet.Address(bytes("mm.slippi.gg", 'utf-8'), 43113), 1)
+print("Connecting to MM server...")
 
 event = host.service(1000)
 if event.type == enet.EVENT_TYPE_CONNECT:
-    print("connected")
     request = {}
     request["type"] = "create-ticket"
     request["user"] = {"uid": user_json["uid"], "playKey": user_json["playKey"]}
     request["search"] = {"mode": 2, "connectCode": [ord(s) for s in args.opponent]}
-    # request["search"] = {"mode": 1}
     request["appVersion"] = "2.3.1"
-    print(request)
     peer.send(0, enet.Packet(bytes(json.dumps(request), 'utf-8')))
 else:
-    print("Not conneced")
+    print("Failed to get ticket from MM server")
     sys.exit(1)
 
 # Wait for match with opponent
 while True:
     event = host.service(2000)
     event_type = event.type
-    print(event_type)
 
     if event.type == enet.EVENT_TYPE_NONE:
-        print("nothing...")
+        print("Waiting to be connected with opponent...")
     elif event.type == enet.EVENT_TYPE_RECEIVE:
-        print("CONNECTED")
         response_ticket = json.loads(event.packet.data)
         print(response_ticket)
         if response_ticket["type"] == "get-ticket-resp":
@@ -112,7 +136,6 @@ while True:
         print("how")
     elif event.type == enet.EVENT_TYPE_DISCONNECT:
         print("Disconnecting")
-
 
 # Now we connect to the opponent directly
 peer.disconnect()
@@ -128,30 +151,30 @@ for player in response_ticket["players"]:
     else:
         our_port = player["ipAddress"].split(":")[1]
 
-print(int(our_port))
-
 host = enet.Host(enet.Address(b"0.0.0.0", int(our_port)), 1, 0, 0)
 peer = host.connect(enet.Address(bytes(opponent_ip, 'utf-8'), int(opponent_port)), 1)
 
 # Wait for match with opponent
 connected = False
+wait_time = 2000
 while True:
-    event = host.service(2000)
+    if connected:
+        wait_time = 50
+    event = host.service(wait_time)
     event_type = event.type
 
     if connected:
-        print("sending selections")
-        selections = PlayerSelections()
-        selections.randomize()
-        peer.send(0, enet.Packet(selections.to_buffer()))
+        chat = ChatMessage()
+        chat.randomize()
+        peer.send(0, enet.Packet(chat.to_buffer()))
 
-    print(event_type)
     if event.type == enet.EVENT_TYPE_NONE:
-        print("nothing...")
+        pass
     elif event.type == enet.EVENT_TYPE_RECEIVE:
         print("Data")
     elif event.type == enet.EVENT_TYPE_CONNECT:
-        print("CONNECTED TO OPPONENT!")
+        print("Connected to opponent!")
+        print("Fuzzing messages...")
         connected = True
     elif event.type == enet.EVENT_TYPE_DISCONNECT:
         print("Disconnecting")
